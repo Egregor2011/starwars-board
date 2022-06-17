d9035879a4b0a829e4c459afb9dcda5eee933eb7
Egregor2011 <egregor2011@gmail.com>
2022-06-17 13:54:35 +0100

    setting up esconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,8 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'plugin:sonarjs/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -15,10 +17,26 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
+    '@typescript-eslint/no-var-requires': 'off',
+    'import/prefer-default-export': 'warn',
+    'import/extensions': 'off',
+    camelcase: 'off',
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-uses-react': 0,
+    'import/no-unresolved': 'off',
+    'import/no-useless-path-segments': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', 'ts', 'tsx'] },
+    ],
+    'object-curly-newline': 'off',
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+      },
+    ],
   },
 };
